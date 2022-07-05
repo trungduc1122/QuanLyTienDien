7d842f11a2564a8bb844494f25a5dd415f394276
trungduc1122 <100517525+trungduc1122@users.noreply.github.com>
2022-07-05 12:39:45 +0700

    update 2

--- Object/Customer.cs
+++ Object/Customer.cs
@@ -1,4 +1,4 @@
-﻿namespace Object
+﻿namespace QLTDLIbrary
 {
     public class Customer
     {
@@ -6,25 +6,54 @@
         {
         }
 
-        public string id { get; set; }
-        public string name { get; set; }
-        public string address { get; set; }
-        public int quantity { get; set; }
-        public int price { get; set; }
-        public string nationality { get; set; }
-        public string type { get; set; }
-        public int limit { get; set; }
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Quantity { get; set; }
+        public int Price { get; set; }
+        public string Type { get; set; }
+        public int Limit { get; set; }
 
-        public Customer(string id, string name, string address, int quantity, int price, string nationality, string type, int limit)
+        public Customer(string id, string name, string address, int quantity, int price, string type, int limit)
         {
-            this.id = id;
-            this.name = name;
-            this.address = address;
-            this.quantity = quantity;
-            this.price = price;
-            this.nationality = nationality;
-            this.type = type;
-            this.limit = limit;
+            Id = id;
+            Name = name;
+            Address = address;
+            Quantity = quantity;
+            Price = price;
+            Type = type;
+            Limit = limit;
+        }
+
+        public void Input()
+        {
+            Console.WriteLine("id:");
+            Id = Console.ReadLine();
+            Console.WriteLine("name:");
+            Name = Console.ReadLine();
+            Console.WriteLine("address:");
+            Address = Console.ReadLine();
+            Console.WriteLine("quantity:");
+            Quantity = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("price:");
+            Price = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("type:");
+            Type = Console.ReadLine();
+            Console.WriteLine("limit:");
+            Limit = Convert.ToInt32(Console.ReadLine());
+        }
+        public double ToCash()
+        {
+            double total;
+            if (Quantity <= Limit)
+            {
+                total=Quantity*Price;
+            }
+            else
+            {
+                total = Quantity * Price * Limit + (Quantity - Limit) * Price * 2.5;
+            }
+            return total;
         }
     }
 }
