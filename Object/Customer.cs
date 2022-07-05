3378375bdce6396558719cc1eded7160660798d8
trungduc1122 <100517525+trungduc1122@users.noreply.github.com>
2022-07-05 11:01:27 +0700

    update library

--- Object/Customer.cs
+++ Object/Customer.cs
@@ -2,23 +2,29 @@
 {
     public class Customer
     {
+        public Customer()
+        {
+        }
+
         public string id { get; set; }
         public string name { get; set; }
         public string address { get; set; }
         public int quantity { get; set; }
         public int price { get; set; }
+        public string nationality { get; set; }
+        public string type { get; set; }
+        public int limit { get; set; }
 
-        public Customer(string id, string name, string address, int quantity, int price)
+        public Customer(string id, string name, string address, int quantity, int price, string nationality, string type, int limit)
         {
             this.id = id;
             this.name = name;
             this.address = address;
             this.quantity = quantity;
             this.price = price;
-        }
-
-        public Customer()
-        {
+            this.nationality = nationality;
+            this.type = type;
+            this.limit = limit;
         }
     }
 }
